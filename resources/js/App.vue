af792c8b570a2ba27829268d1957b714f957039f
mercExec <defaultg123@gmail.com>
2023-11-26 06:37:52 +0300

    Added interceptor to axios to deal with 401 errors

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,6 +3,20 @@
 </template>
 
 <script setup>
+import axios from "axios";
+import { useAuthStore } from "./stores/auth";
+
+const authStore = useAuthStore();
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+        authStore.handleLogout();
+    }
+    return Promise.reject(error);
+  }
+);
 
 </script>
 <style></style>
