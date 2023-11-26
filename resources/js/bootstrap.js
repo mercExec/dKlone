75f3b5d72a89f53dafc0113df933c7e3156bd7b1
mercExec <defaultg123@gmail.com>
2023-11-26 05:51:47 +0300

    bootstrap.js should no longer be tracked

--- resources/js/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8000";
